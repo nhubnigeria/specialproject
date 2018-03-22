1852ef863a067083ca50a3d3047b8c85e4141fa8
vixxjy <ajorvictor48@gmail.com>
2018-03-22 16:35:15 +0100

    initial commit

---
+++ DOMStructureService/betfair/utils/sendMail.js
@@ -0,0 +1,57 @@
+'use strict';
+/**
+ * Module dependencies
+ */
+//=============================================================================
+const
+  nodemailer = require('nodemailer'),
+  // sgTransport = require('nodemailer-sendgrid-transport');
+  sgTransport = require('nodemailer-smtp-transport');
+//=============================================================================
+/**
+ * Module variables
+ */
+//=============================================================================
+const
+  // sgtOptions = {
+
+  //   auth: {
+  //     api_user: 'nanipaul',
+  //     api_pass: 'p@55w0rd!'
+  //   }
+  // },
+ 
+  sgtOptions = {
+    service: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+      user: '',
+      pass: ''
+    }
+  },
+  mailer = nodemailer.createTransport(sgTransport(sgtOptions));
+
+//===================================`==========================================
+/**
+ * Export Module
+ */
+//=============================================================================
+module.exports = function (details, topic) {
+  const msg = {
+    to: 'vixxjy@gmail.com',
+    from: 'report@botservice.com',
+    subject: topic,
+    html: `<div><p>Hello Ajor,</p><p>${topic}</p>
+           <br><ul><b>${details}</b></ul>
+          <p>Have a splendid day</p>
+    </div>`
+  };
+  //send email
+  mailer.sendMail(msg, function (err, res) {
+    if (err) {
+      return console.error(err);
+    }
+    return console.log(res);
+  });
+};
+//=============================================================================
